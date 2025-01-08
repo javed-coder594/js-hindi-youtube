e8ddf276ddcf0fa3af6dff148dbe42f2fcb9810f
javed-coder594 <152312307+javed-coder594@users.noreply.github.com>
2025-01-08 06:31:10 +0000

    conversion

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -8,14 +8,35 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
 
-// === 
+// // === 
 
-console.log("2" === 2);
+// console.log("2" === 2);
+
+let score ="33abc"
+console.log (typeof (score))
+let scoretonum = Number(score)
+console.log ( typeof(scoretonum))
+console.log (scoretonum)
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn ="0"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let num =33
+let numtostr = String (num)
+console.log(typeof(numtostr))

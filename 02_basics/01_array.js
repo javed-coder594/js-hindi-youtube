22ba38d4006fb611e9b2765bf83171685a9814ea
javed-coder594 <152312307+javed-coder594@users.noreply.github.com>
2025-01-22 06:43:52 +0000

    array1

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -15,28 +15,35 @@ const myarr =[0,1,2,3,4,5]
 
 // *******push******** Add last element
 myarr.push(6)
+//o/p >>>> [0,1,2,3,4,5,6]
 
 // ******pop****** Remove last element
 myarr.pop()
+//o/p >>>> [0,1,2,3,4,5]
 
 // ******unshift****** add starting element
 myarr.unshift(9)
+//o/p >>>> [9,0,1,2,3,4,5,6]
 
 // ******shift****** remove starting element
 myarr.shift()
+//o/p >>>> [0,1,2,3,4,5,6]
 
 // console.log(myarr);
 
 // for indexof
 // console.log(myarr.indexOf(3)); 
+// o/p >>>>> 4
 
 // this Element is available or not 
 // console.log(myarr.includes(3));
+// o/p>>> true
 
 // join () convert array into string 
 // const newarr = myarr.join()
 // console.log(newarr);
 // console.log(typeof(newarr));
+//o/p >>>> 0,1,2,3,4,5,6
  
 
 
@@ -46,16 +53,21 @@ myarr.shift()
 // // If we go trought slice in original array will same and index of 3 rd will not print  
 const arr =[0,1,2,3,4,5,6]
 console.log("A", myarr);
+//o/p>>>>> A [ 0, 1, 2, 3, 4, 5 ]
 const myna1= arr.slice(1,3)
 console.log(myna1);
+// o/p >>> [ 1, 2 ]
 
 
 // *****splice******
 // If we go trought splice in original array will manipulate 
 console.log("B", myarr);
+// o/p >>>>> B [ 0, 1, 2, 3, 4, 5 ]
 const myna2 = arr.splice(1,3)
 console.log(arr)
+// o/p >>>>>> [ 0, 4, 5, 6 ]
 console.log(myna2);
+// o/p >>>>>> [ 1, 2, 3 ]
 
 
 
